a595e98d810b4a0715de8fc7bc2e4397387ac8bf
Appoline Cogan <appolinecogan@gmail.com>
2023-01-24 14:35:38 +1100

    added uv colorchange function

--- src/component/CurrentWeather/UvIndex/index.js
+++ src/component/CurrentWeather/UvIndex/index.js
@@ -1,9 +1,22 @@
+import React from "react";
 import { VStack, Text, useColorModeValue } from "@chakra-ui/react";
 import { BsSunFill } from "react-icons/bs";
 
 export default function Wind(props) {
   const text = useColorModeValue("#155264", "white");
-  const data = useColorModeValue("#FD6F3B", "gray.300");
+  // const data = useColorModeValue("#FD6F3B", "gray.300");
+
+  const colorChange = (value) => {
+    let color;
+    if (value >= 0 && value <= 3) {
+      color = "green";
+    } else if (value >= 4 && value <= 9) {
+      color = "yellow";
+    } else {
+      color = "red";
+    }
+    return color;
+  };
 
   return (
     <VStack alignItems="center" pt={4} spacing="20px">
@@ -11,7 +24,12 @@ export default function Wind(props) {
       <Text alignSelf={"center"} fontWeight="600" color={text} fontSize={"xl"}>
         Uv index
       </Text>
-      <Text alignSelf={"center"} fontWeight="600" color={data} fontSize={"lg"}>
+      <Text
+        alignSelf={"center"}
+        fontWeight="600"
+        color={colorChange(props.data)}
+        fontSize={"lg"}
+      >
         {Math.round(props.data)}
       </Text>
     </VStack>
